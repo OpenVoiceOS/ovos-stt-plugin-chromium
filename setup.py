fb49b7878fa62ae31c58a0d6c1e2f9ea046f7f21
miro <jarbasai@mailfence.com>
2024-09-10 21:53:39 +0100

    cgore:automations

--- setup.py
+++ setup.py
@@ -1,8 +1,35 @@
 #!/usr/bin/env python3
 from setuptools import setup
-
+import os
 PLUGIN_ENTRY_POINT = 'ovos-stt-plugin-chromium = ovos_stt_plugin_chromium:ChromiumSTT'
 CONFIG_ENTRY_POINT = 'ovos-stt-plugin-chromium.config = ovos_stt_plugin_chromium:ChromiumSTTConfig'
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join('ovos_stt_plugin_chromium', 'version.py')
+    print(f"ERROR: version file: {version_file}")
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+
 
 setup(
     name='ovos-stt-plugin-chromium',
